6ceb309f5e123c75e8c9d9b0fb7262c8a1404545
kierawilson25 <60899868+kierawilson25@users.noreply.github.com>
2020-04-05 19:16:16 -0500

    Update transforms.js
    Perspective projection

--- scripts/transforms.js
+++ scripts/transforms.js
@@ -39,16 +39,40 @@ function Mat4x4Parallel(mat4x4, prp, srp, vup, clip) {
     mat4x4.values = transform.values;
 }
 
-// set values of mat4x4 to the parallel projection / view matrix
-function Mat4x4Projection(mat4x4, prp, srp, vup, clip) {
-    // 1. translate PRP to origin
+// set values of mat4x4 to the perspective projection / view matrix
+function Mat4x4Perspective(mat4x4, prp, srp, vup, clip) {
+     // 1. translate PRP to origin
+    var translateOrg = new Matrix(4,4); //create new matrix
+    Mat4x4Translate(translateOrg, -prp.x, -prp.y, -prp.z); //translare prp to origin
     // 2. rotate VRC such that (u,v,n) align with (x,y,z)
-    // 3. shear such that CW is on the z-axis
-    // 4. scale such that view volume bounds are ([z,-z], [z,-z], [-1,zmin])
+    var rotateVRC = new Matrix(4,4);
+    var n = prp.subtract(srp).normalize();
+    var u = vup.cross(n).normalize();
+    var v - n.cross(u);
+    rotateVRC.values = [[u.x, u.y, u.z, 0],
+                        [v.x, v.y, v.z, 0],
+                        [n.x, n.y, n.z, 0],
+                        [  0,   0,   0, 1]];
 
+    // 3. shear such that CW is on the z-axis
+    var shearCW = new Matrix(4,4);
+    var CW = Vector3((clip[0]+clip[1])/2, (clip[2]+clip[3])/2);
+    var DOP = cw.subtract(prp);
+    var shx = -DOP.x/DOP.z;
+    var shy = -DOP.y/DOP.z;
+    Mat4x4ShearXY(shearCW, shx, shy);
+    // 4. scale such that view volume bounds are ([z,-z], [z,-z], [-1,zmin])
+    var scaleView = new Matrix(4,4);
+    var sPerX = (2*clip[4])/(clip[1].subtract(clip[0])*clip[5]);
+    var sPerY = (2*clip[4])/(clip[3].subtract(clip[2])*clip[5]);
+    var sperZ = 1/ clip[5];
+
+    Mat4x4Scale(scaleView, sPerX, sPerY, sPerZ);
     // ...
     // var transform = Matrix.multiply([...]);
+    var transform = Matrix.multiply([scaleView, shearCW, rotateVRC, translateOrg]);
     // mat4x4.values = transform.values;
+    mat4x4.values = transform.values;
 }
 
 // set values of mat4x4 to project a parallel image on the z=0 plane
@@ -62,6 +86,11 @@ function Mat4x4MPar(mat4x4) {
 // set values of mat4x4 to project a perspective image on the z=-1 plane
 function Mat4x4MPer(mat4x4) {
     // mat4x4.values = ...;
+    //check if these are the right matrix values
+    mat4x4.values = [[1,0,0,0],
+                     [0,1,0,0],
+                     [0,0,1,0],
+                     [0,0,-1,0]];
 }
 
 
